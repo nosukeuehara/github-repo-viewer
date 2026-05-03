933b7d8256c83c0552664524050973258e75d775
nosukeuehara <ryuunosuke06160616@gmail.com>
2026-05-03 19:19:44 +0900

    update

--- src/feature/githubRepository/components/RepositorySearchResultList/RepositorySearchResultListPresentaion.test.tsx
+++ src/feature/githubRepository/components/RepositorySearchResultList/RepositorySearchResultListPresentaion.test.tsx
@@ -1,6 +1,5 @@
 import {render, screen} from "@testing-library/react";
 import {RepositoryListPresentation} from "./RepositorySearchResultListPresentation";
-import "@testing-library/jest-dom/vitest";
 
 const repositories = [
   {
