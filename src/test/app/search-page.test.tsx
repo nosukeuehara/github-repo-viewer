e118d925ce637dfb1644a45bc429080159956573
nosukeuehara <ryuunosuke06160616@gmail.com>
2026-05-06 20:41:40 +0900

    delete : 不要なファイル削除

--- src/test/app/search-page.test.tsx
+++
@@ -1,6 +0,0 @@
-import {render, screen} from "@testing-library/react";
-import {RepositorySearch} from "@/template/RepositorySearchTemplate/RepositorySearchTemplate";
-
-describe("SearchPage", () => {
-  it.todo("検索結果を表示する", async () => {});
-});
