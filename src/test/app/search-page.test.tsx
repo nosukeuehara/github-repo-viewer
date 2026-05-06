0a67c9faa67bc73ec85a292d60ae26e2141cc325
nosukeuehara <ryuunosuke06160616@gmail.com>
2026-05-06 19:49:33 +0900

    テストファイル追加

---
+++ src/test/app/search-page.test.tsx
@@ -0,0 +1,6 @@
+import {render, screen} from "@testing-library/react";
+import {RepositorySearch} from "@/template/RepositorySearchTemplate/RepositorySearchTemplate";
+
+describe("SearchPage", () => {
+  it.todo("検索結果を表示する", async () => {});
+});
